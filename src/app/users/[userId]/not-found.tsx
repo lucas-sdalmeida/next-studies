b4637571a4ab559b206d3c5ce9d1e2640bf61639
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2024-05-26 18:42:15 -0300

    Cleared main branch to keep all lessons in separate branches

--- src/app/users/[userId]/not-found.tsx
+++
@@ -1,3 +0,0 @@
-export default function UserNotFound() {
-    return <h1>The requested user has not been found!</h1>
-}
--- src/lib/get-posts-by-user.tsx
+++
@@ -1,7 +0,0 @@
-export default async function getPostsByUser(userId: string) {
-    const response = await fetch(
-        `https://jsonplaceholder.typicode.com/users/${userId}/posts`,
-        { next: { revalidate: 60 } },
-    )
-    return response.ok ? response.json() : undefined
-}
